d19ff3918eef7e14b1ff8c1ad3ede1cedf61c016
Mathieu <msaby@users.noreply.github.com>
2017-06-02 00:13:38 +0200

    site

--- docs/assets/TEI-Boilerplate/dist/js/teibp.js
+++ docs/assets/TEI-Boilerplate/dist/js/teibp.js
@@ -1,1 +1,106 @@
-function clearPageBreaks(){for(var e=document.querySelectorAll("pb"),t=0;t<e.length;t++)e[t].style.display="none";for(var e=document.querySelectorAll(".-teibp-pb"),t=0;t<e.length;t++)e[t].style.display="none"}function addPageBreaks(){for(var e=document.querySelectorAll("pb"),t=0;t<e.length;t++)e[t].style.display="block";for(var e=document.querySelectorAll(".-teibp-pb"),t=0;t<e.length;t++)e[t].style.display="block"}function init(){var e=document.getElementById("pbToggle");null!=e&&(e.onclick=function(){this.checked?clearPageBreaks():addPageBreaks()},addPageBreaks(),document.getElementById("pbToggle").checked=!1);var t=document.querySelector("html > head > title"),n=document.querySelector("tei-title");null!=t&&null!=n&&(t.textContent=n.textContent)}function blockUI(){var e=document.querySelector("body"),t=document.createElement("div");t.setAttribute("class","blocker"),e.appendChild(t)}function unblockUI(){var e=document.querySelector(".blocker");e&&e.parentNode.removeChild(e)}function switchThemes(e){document.getElementById("maincss").href=e.options[e.selectedIndex].value}function showFacs(e,t,n){for(var o="",c=document.querySelectorAll(".-teibp-thumbnail"),r=0;r<c.length;r++)o+="<img id='"+c[r].parentNode.parentNode.parentNode.getAttribute("id")+"' src='"+c[r].getAttribute("src")+"' alt='facsimile page image'/>";var l=["<html>","<head>","<title></title>",document.querySelector("#maincss").outerHTML,document.querySelector("#customcss").outerHTML,null!=document.querySelector("#teibp-tagusage-css")?document.querySelector("#teibp-tagusage-css").outerHTML:"",null!=document.querySelector("#teibp-rendition-css")?document.querySelector("#teibp-rendition-css").outerHTML:"","<script src='../js/teibp.js'></script>","</head>","<body>","<script>blockUI();</script>",document.querySelector("teiHeader").outerHTML,"<div id='resizable'>","<div class='facsImage'>",o,"</div>","</div>",document.querySelector("footer").outerHTML,"<script>","document.getElementById('"+n+"').scrollIntoView();","unblockUI();","</script>","</body>","</html>"].join("\n");facsWindow=window.open("about:blank"),facsWindow.document.write(l),facsWindow.document.close()}document.addEventListener?(document.addEventListener("DOMContentLoaded",init,!1),window.addEventListener("load",init,!1)):document.attachEvent&&(document.attachEvent("onreadystatechange",init),window.attachEvent("onload",init));
+//If W3C event model used, prefer that. Window events are fallbacks
+if(document.addEventListener){
+	//W3C event model used
+	document.addEventListener("DOMContentLoaded", init, false);
+	window.addEventListener("load", init, false);
+} else if(document.attachEvent){
+	//IE event model used
+	document.attachEvent( "onreadystatechange", init);
+	window.attachEvent( "onload", init);
+}
+
+function clearPageBreaks(){
+	var breaks = document.querySelectorAll("pb");
+	for(var i = 0; i < breaks.length; i++)
+		breaks[i].style.display="none";
+	
+	var breaks = document.querySelectorAll(".-teibp-pb");
+	for(var i = 0; i < breaks.length; i++)
+		breaks[i].style.display="none";
+}
+
+function addPageBreaks(){
+	var breaks = document.querySelectorAll("pb");
+	for(var i = 0; i < breaks.length; i++)
+		breaks[i].style.display="block";
+
+	var breaks = document.querySelectorAll(".-teibp-pb");
+	for(var i = 0; i < breaks.length; i++)
+		breaks[i].style.display="block";
+}
+
+function init(){
+	var pbt = document.getElementById('pbToggle');
+	if(pbt != null){
+		pbt.onclick = function(){
+			if(this.checked)
+				clearPageBreaks();
+			else
+				addPageBreaks();
+		};
+	
+		addPageBreaks();
+		document.getElementById('pbToggle').checked = false;
+	}
+	
+	var htmlTitle = document.querySelector("html > head > title");
+	var teiTitle = document.querySelector("tei-title");
+	if(htmlTitle != null && teiTitle != null)
+		htmlTitle.textContent = teiTitle.textContent;
+}
+
+function blockUI(){
+	var body = document.querySelector("body");
+	var blocker = document.createElement('div');
+	blocker.setAttribute('class', 'blocker');
+	body.appendChild(blocker);	
+}
+
+function unblockUI(){
+	var blocker = document.querySelector(".blocker");
+	if(blocker)
+		blocker.parentNode.removeChild(blocker)
+}
+
+function switchThemes(theme) {
+	document.getElementById('maincss').href=theme.options[theme.selectedIndex].value;
+}
+
+function showFacs(num, url, id) {
+	var imgs = "";
+	var thumbs = document.querySelectorAll(".-teibp-thumbnail");
+	for(var i = 0; i < thumbs.length; i++){
+		imgs+= "<img id='" + thumbs[i].parentNode.parentNode.parentNode.getAttribute("id") + "' src='" + thumbs[i].getAttribute("src") + "' alt='facsimile page image'/>";
+	}	
+	var html = [
+		"<html>",
+			"<head>",
+				"<title></title>",
+				document.querySelector('#maincss').outerHTML,
+				document.querySelector('#customcss').outerHTML,
+				document.querySelector('#teibp-tagusage-css') != null ? document.querySelector('#teibp-tagusage-css').outerHTML : "",
+				document.querySelector('#teibp-rendition-css') != null ? document.querySelector('#teibp-rendition-css').outerHTML : "",
+				"<script src='../js/teibp.js'></script>",
+			"</head>",
+			"<body>",
+				"<script>blockUI();</script>",
+				document.querySelector("teiHeader").outerHTML,
+				"<div id='resizable'>",
+					"<div class='facsImage'>",
+						imgs,
+					"</div>",
+				"</div>",
+				document.querySelector("footer").outerHTML,
+				
+				"<script>",
+					"document.getElementById('" + id + "').scrollIntoView();",
+					"unblockUI();",
+				"</script>",
+			"</body>",
+		"</html>",	
+	].join('\n');
+	
+	facsWindow = window.open ("about:blank");
+	facsWindow.document.write(html);
+	facsWindow.document.close();
+}
