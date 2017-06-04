e8472cfbe8d605c08384dfe4fdcbe057c686647f
Mathieu <msaby@users.noreply.github.com>
2017-06-04 17:06:24 +0200

    site

--- docs/assets/TEI-Boilerplate/dist/js/teibp.js
+++ docs/assets/TEI-Boilerplate/dist/js/teibp.js
@@ -212,6 +212,7 @@ if (i==(menuitems.length-1)){
 	html2 = "<ul>	<li>1<a href='#id0x1aba6700' id='link-id0x1aba6700'>Texte urn:cts:greekLit:tlg0086.tlg014.1st1K-grc1-zoom1</a> <ul><li>2<a href='#id0x1acdfe00' id='link-id0x1acdfe00'>Livre 1</a>					<ul>						<li>3<a href='#id0x1ace0000' id='link-id0x1ace0000'>Chapitre 1</a></li>						<li>3<a href='#id0x1bc5cb00' id='link-id0x1bc5cb00'>Chapitre 2</a></li>						<li>3<a href='#id0x1bc5cf00' id='link-id0x1bc5cf00'>Chapitre 3</a></li>					</ul>				</li>				<li>2<a href='#id0x1bc64700' id='link-id0x1bc64700'>Livre 2</a><ul>	<li>3<a href='#id0x1bce1200' id='link-id0x1bce1200'>Chapitre 4</a></li>	</ul>				</li>			</ul>	</li></ul>";
 	
 	$("#navigation_tree").append(html);
+	$("#navigation_tree").ntm();
 
 //   	// <div class='tree-menu menu' id='navigation_tree'>\n
 // var toc = $("#navigation_tree").tocify({ selectors:"div[type='edition'],div[type='textpart'][subtype='book'],div[type='textpart'][subtype='chapter']"});
